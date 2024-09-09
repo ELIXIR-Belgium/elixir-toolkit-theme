0f7513574e0cbc62a03ea943d5968324ce91995d
bedroesb <bert.droesbeke@vib.be>
2024-09-09 13:52:08 +0200

    improved version

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -2,7 +2,6 @@
 (function ($) {
   $.fn.toc = function (options) {
     var defaults = {
-      noBackToTopLinks: false,
       title: '<i>Jump to...</i>',
       minimumHeaders: 3,
       headers: 'h1, h2, h3, h4, h5, h6',
@@ -35,7 +34,9 @@
         this.id = $(this).text().trim().replace(/\s+/g, '-').toLowerCase();
       }
       return this.id;
-    }), output = $(this);
+    });
+
+    var output = $(this);
 
     // Check if there are any headers
     if (!headers.length || headers.length < settings.minimumHeaders || !output.length) {
@@ -59,16 +60,11 @@
 
     var get_level = function (ele) { return parseInt(ele.nodeName.replace("H", ""), 10); };
     var highest_level = headers.map(function (_, ele) { return get_level(ele); }).get().sort()[0];
-    var return_to_top = '<i class="icon-arrow-up back-to-top"> </i>';
+    var level = get_level(headers[0]), this_level;
+    var html = settings.title + " <" + settings.listType + " class=\"" + settings.classes.list + "\">";
 
-    var level = get_level(headers[0]),
-        this_level,
-        html = settings.title + " <" + settings.listType + " class=\"" + settings.classes.list + "\">";
     headers.each(function (_, header) {
       this_level = get_level(header);
-      if (!settings.noBackToTopLinks && this_level === highest_level) {
-        $(header).addClass('top-level-header').after(return_to_top);
-      }
       if (this_level === level) { // same level as before; same indenting
         html += "<li class=\"" + settings.classes.item + "\">" + createLink(header);
       } else if (this_level <= level) { // higher level than before; end parent ol
@@ -86,14 +82,6 @@
       level = this_level; // update for the next one
     });
     html += "</" + settings.listType + ">";
-
-    if (!settings.noBackToTopLinks) {
-      $(document).on('click', '.back-to-top', function () {
-        $(window).scrollTop(0);
-        window.location.hash = '';
-      });
-    }
-
     render[settings.showEffect]();
   };
 })(jQuery);
