b56aa9f2f31b992280645b744a8a8a9f1ef3dcfb
bedroesb <bert.droesbeke@vib.be>
2024-12-10 14:32:28 +0100

    final fix

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -39,10 +39,12 @@
     var output = $(this);
 
     // Check if there are any headers
-    if (!headers.length || headers.length < settings.minimumHeaders || !output.length || $('#page-img').length === 0) {
-      $('#main').removeClass("add-grid");
-      $("#toc").hide();
-      return;  // Exit early if there are no headers
+    if (!$('#page-img').length ) {
+      if ( !headers.length || headers.length < settings.minimumHeaders || !output.length ) {
+        $('#main').removeClass("add-grid");
+        $("#toc").hide();
+        return;  // Exit early if there are no headers
+      }
     }
 
     if (settings.showSpeed === 0) {
